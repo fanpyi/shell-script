fdb31602696c8a089bfb73834de4bab80200680a
fanpyi <fanpyi@163.com>
2015-12-26 16:42:15 +0800

    修改了analyse.sh

--- analyse.sh
+++ analyse.sh
@@ -4,15 +4,13 @@ cd gitlab/ConsumerApp
 
 for path in `find Beequick | grep -E '.*\.[hm]'`; do
 	 s=`echo $path | awk -F '/' '{print $NF}' | grep -E '^BQ.*\.[hm]'` 
-	 len=${#s}
-	 if [[ $len != 0 ]]; then
+	 if [[ x$s != x ]]; then
 	 	cat $path | while read line; do
 	 		l=`echo $line | grep 'assign'`
 	 		if [[ $line == '@end' ]] || [[ $line == "@implementation" ]]; then
 	 			break
 	 		else 
-	 			len=${#l}
-	 			if [[ $len != 0 ]]; then
+	 			if [[ x$l != x ]]; then
 	 				echo "${line}:--------------------$s" >> ~/Desktop/analyse.text
 	 			fi
 	 		fi
--- grep/test.sh
+++ grep/test.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-for ((i=0;i<1000;i++))
-do
-echo "${i}:第${i}行" >>num.txt
+cat num.txt | while read line; do
+	s=`echo $line | grep -E 'hellossss'`
+	if [[ x$s != x ]]; then
+		echo "s=$s"
+	fi
 done
+
+if [[ $flag == 1 ]]; then
+	echo 'exsit hellossss'
+else 
+	echo 'not exsit hellossss'
+fi
+
+echo $?
